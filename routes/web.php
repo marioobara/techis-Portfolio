0976e6a89219a0433fb0c332e11995813a44033f
小原万里雄 <>
2022-11-11 20:44:22 +0900

    登録画面・ログイン画面の変更、ログイン後のリダイレクト編集

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,11 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::fallback(function () {
+    return redirect('/items');
+  });
+
+
 Route::prefix('items')->group(function () {
     Route::get('/', [App\Http\Controllers\ItemController::class, 'index']);
     Route::get('/add', [App\Http\Controllers\ItemController::class, 'add']);
