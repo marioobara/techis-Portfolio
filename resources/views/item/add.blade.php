6d4adce7107af25bb7be2b639b24dfd2b662c5fa
小原万里雄 <>
2022-12-09 20:38:32 +0900

    add,editbladeを統一

--- resources/views/item/add.blade.php
+++ resources/views/item/add.blade.php
@@ -1,31 +1,51 @@
 @extends('adminlte::page')
 
-@section('title', '商品登録')
+@section('title', '登録画面')
 
 @section('content_header')
-    <h1>商品登録</h1>
+
 @stop
+<style>
+    label {
+        text-indent: 1rem;
+        }
+    .items {
+        padding-top: 100px;
+        margin: 0 auto;
+        }
+    .form-group.item {
+        margin-bottom: 1rem;
+    }
+    .btn {
+        margin-top: 30px;
+        padding: 5px 60px;
+        }
+    </style>
 
 @section('content')
-    <div class="row">
-        <div class="col-md-10">
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                       @foreach ($errors->all() as $error)
-                          <li>{{ $error }}</li>
-                       @endforeach
-                    </ul>
-                </div>
-            @endif
+    <div class="items col-md-6">
+        @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                        </ul>
+                    </div>
+                @endif
+            <h1>登録画面</h1>
 
             <div class="card card-primary">
                 <form method="POST">
                     @csrf
                     <div class="card-body">
-                        <div class="form-group">
+
+                        <div class="form-group item">
                             <label for="name">名前</label>
                             <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" placeholder="名前" value="{{old('name')}}">
+                            @error('name')
+                            <p class="text-danger">{{ $message }}</p>
+                            @enderror
                         </div>
 
                         <div class="form-group">
@@ -43,19 +63,28 @@
                         <div class="form-group">
                             <label for="stock">在庫</label>
                             <input type="text" class="form-control @error('stock') is-invalid @enderror" id="stock" name="stock" placeholder="在庫数" value="{{old('stock')}}">
+                            @error('stock')
+                            <p class="text-danger">{{ $message }}</p>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="detail">詳細</label>
                             <input type="text" class="form-control" id="detail" name="detail" placeholder="詳細説明">
+                            @error('detail')
+                            <p class="text-danger">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="card-footer">
+                        <!-- 編集ボタン -->
                         <button type="submit" class="btn btn-primary">登録</button>
                     </div>
                 </form>
             </div>
-        </div>
+
+
+        </form>
     </div>
 @stop
 
--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -1,7 +1,10 @@
 @extends('adminlte::page')
 
-@section('title', '商品一覧')
+@section('title', '編集画面')
 
+@section('content_header')
+
+@stop
 <style>
     label {
         text-indent: 1rem;
@@ -21,44 +24,70 @@
 
 @section('content')
 <div class="items col-md-6">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        </ul>
+    </div>
+    @endif
     <form action="{{ url('itemEdit') }}" method="POST">
         @csrf
         <!-- 編集フォーム -->
-        <input class="form-control" type="text" name="id" value="{{ $item->id }}" hidden>
-        <div class="form-group item">
-            <label for="name">名前</label>
-            <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $item->name) }}">
-            @error('name')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
+        <h1>編集画面</h1>
+
+        <div class="card card-primary">
+            <form method="POST">
+                @csrf
+                <div class="card-body">
+
+                    <input class="form-control" type="text" name="id" value="{{ $item->id }}" hidden>
+                    <div class="form-group item">
+                        <label for="name">名前</label>
+                        <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $item->name) }}">
+                        @error('name')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="type">種別</label>
+                        <select type="text" name="type" class="form-control" required>
+                            @foreach (Config::get('const.type') as $key => $val)
+                                <option value="{{ $key }}" @if( old('type')== $key) selected @endif>{{ $val }}</option>
+                            @endforeach
+                            @error('type')
+                            <p class="text-danger">{{ $message }}</p>
+                            @enderror
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="stock">在庫</label>
+                        <input type="text" class="form-control" id="stock" name="stock" value="{{ old('stock', $item->stock) }}" placeholder="在庫数">
+                        @error('stock')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="detail">詳細</label>
+                        <input type="text" name="detail" id="detail" class="form-control" value="{{ old('detail', $item->detail) }}">
+                        @error('detail')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <!-- 編集ボタン -->
+                    <button type="submit" class="btn btn-primary">更新</button>
+                </div>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="type">種別</label>
-            <select type="text" name="type" class="form-control" required>
-                @foreach (Config::get('const.type') as $key => $val)
-                    <option value="{{ $key }}" @if( old('type')== $key) selected @endif>{{ $val }}</option>
-                @endforeach
-                @error('type')
-                <p class="text-danger">{{ $message }}</p>
-                @enderror
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="stock">在庫</label>
-            <input type="text" class="form-control" id="stock" name="stock" value="{{ old('stock', $item->stock) }}" placeholder="在庫数">
-            @error('stock')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
-        <div class="form-group item">
-            <label for="detail">詳細</label>
-            <input type="text" name="detail" id="detail" class="form-control" value="{{ old('detail', $item->detail) }}">
-            @error('detail')
-            <p class="text-danger">{{ $message }}</p>
-            @enderror
-        </div>
-        <!-- 編集ボタン -->
-            <button type="submit" class="btn btn-outline-secondary">更新</button>
+
+
     </form>
 </div>
 @stop
